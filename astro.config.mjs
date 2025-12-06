eeee60ace2a77bf9392ad7d2332bd3690f5eb864
cgdops <christian@vantage-communications.com>
2025-12-06 14:57:15 -0500

    Update to astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,1 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  // Add Tailwind support
-  integrations: [
-    tailwind(),
-    sitemap()
-  ],
-
-  // vital for GitHub Pages hosting
-  site: 'https://cgdops.github.io',
-  base: '/g3z-agency-template-v1', 
-});
+import { defineConfig } from 'astro/config'; import tailwind from '@astrojs/tailwind';  export default defineConfig({   integrations: [tailwind()],   site: 'https://cgdops.github.io',   base: '/client-iqguides.com',});
